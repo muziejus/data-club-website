c51c907b37082b72c32113a8ce6cec8819ac1e95
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-25 02:23:16 -0400

    feat: Move graphql query for title to Header component and clean up typing.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,44 +1,17 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+import { GatsbyNode } from "gatsby";
 
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createFieldExtension, createTypes } = actions
-  createFieldExtension({
-    name: `defaultArray`,
-    extend() {
-      return {
-        resolve(source, args, context, info) {
-          if (source[info.fieldName] == null) {
-            return []
-          }
-          return source[info.fieldName]
-        },
-      }
-    },
-  })
-  const typeDefs = `
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] = ({ actions }) => {
+  actions.createTypes(`
     type Site implements Node {
-      siteMetadata: SiteMetadata
+      siteMetadata: SiteMetadata!
     }
     type SiteMetadata {
-      menuLinks: [MenuLinks]!
+      title: String!
+      menuLinks: [MenuLinks!]!
     }
     type MenuLinks {
       name: String!
       link: String!
-      subMenu? [SubMenu] @defaultArray
     }
-    type SubMenu {
-      name: String
-      link: String
-    }
-  `
-  createTypes(typeDefs)
+  `)
 }
-
-
-
-
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,9 +5,8 @@
  * See: https://www.gatsbyjs.com/docs/static-query/
  */
 
-import React from "react"
+import * as React from "react"
 import PropTypes, { InferProps } from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 
@@ -17,45 +16,26 @@ const LayoutPropTypes = {
 
 type LayoutProps = InferProps<typeof LayoutPropTypes>;
 
-const Layout = ({ children }: LayoutProps) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            menuLinks {
-              name
-              link
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-        <>
-        <Header 
-          menuLinks={data.site.siteMetadata.menuLinks} 
-          siteTitle={data.site.siteMetadata.title} 
-        />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.com">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+const Layout: React.FC<LayoutProps> = ({ children }) =>
+(
+  <>
+    <Header />
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }}
+    >
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </div>
+  </>
 )
 
 Layout.propTypes = LayoutPropTypes;
